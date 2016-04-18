2e2a00eb4344b1078ba1f03eeaf120f8ae30ef8c
jeremynoel476@gmail.com <jeremynoel476@gmail.com>
2016-04-18 14:38:45 +0300

    move motor to target percentage

--- control.py
+++ control.py
@@ -21,19 +21,29 @@ cmd_get_battery = "get_battery"
 cmd_get_position = "get_position"
 cmd_move_up = "move_up"
 cmd_move_down = "move_down"
+cmd_move_target = "move_target"
 
 # options
 parser = argparse.ArgumentParser(description=script_dec, version=str(script_ver))
-parser.add_argument("-t", "--target", dest="target", action="store", help="target peripheral MAC address", required=True)
-parser.add_argument("-c", "--command", dest="command", action="store", choices=[cmd_get_battery, cmd_get_position, cmd_move_up, cmd_move_down], help="command to execute on the peripheral", required=True)
-parser.add_argument("-e", "--verbose", dest="verbose", action="store_true", default=False,help="print script execution information", required=False)
+parser.add_argument("-t", "--target", dest="target", action="store", help="target peripheral MAC address",
+                    required=True)
+parser.add_argument("-c", "--command", dest="command", action="store",
+                    choices=[cmd_get_battery, cmd_get_position, cmd_move_up, cmd_move_down, cmd_move_target],
+                    help="command to execute on the peripheral", required=True)
+parser.add_argument("-a", "--motor_target", dest="motor_target", action="store", type=int,
+                    help="value in range 0 - 100, won't move if the distance is too short, set when command is " +
+                         cmd_move_target, required=False)
+parser.add_argument("-e", "--verbose", dest="verbose", action="store_true", default=False,
+                    help="print script execution information", required=False)
 
 args = parser.parse_args()
 # verbose print
 if args.verbose:
-    def printv(string): print(string)
+    def printv(string):
+        print(string)
 else:
-    def printv(string): return
+    def printv(string):
+        return
 
 # battery
 BATTERY_SERVICE_UUID = "0000180f-0000-1000-8000-00805f9b34fb".lower()
@@ -42,8 +52,10 @@ BATTERY_CHARACTERISTIC_UUID = "00002a19-0000-1000-8000-00805f9b34fb".lower()
 MOTOR_SERVICE_UUID = "00001861-B87F-490C-92CB-11BA5EA5167C".lower()
 MOTOR_STATE_CHARACTERISTIC_UUID = "00001525-B87F-490C-92CB-11BA5EA5167C".lower()
 MOTOR_CONTROL_CHARACTERISTIC_UUID = "00001530-B87F-490C-92CB-11BA5EA5167C".lower()
+MOTOR_TARGET_CHARACTERISTIC_UUID = "00001526-B87F-490C-92CB-11BA5EA5167C".lower()
 MOTOR_MOVE_UP = 69
 MOTOR_MOVE_DOWN = 96
+
 
 # peripheral control methods
 def connect(stdin):
@@ -86,8 +98,9 @@ output_list = list()
 service_dict = dict()
 char_dict = dict()
 
-def parse_output(stdout): # read output on a background thread and store it
-    while(True):
+
+def parse_output(stdout):  # read output on a background thread and store it
+    while (True):
         try:
             output_list.append(stdout.readline())
         except Exception:
@@ -122,7 +135,7 @@ def parse_services():
                     print(last_line)
                 return False
             elif "attr handle" in last_line:
-                time.sleep(1) # wait a second for services to be fully printed
+                time.sleep(1)  # wait a second for services to be fully printed
                 for line in output_list[::-1]:
                     if "primary" in line.lower():
                         break
@@ -149,7 +162,7 @@ def parse_chars():
                     print(last_line)
                 return False
             elif "char value handle" in last_line:
-                time.sleep(1) # wait a second for characteristics to be fully printed
+                time.sleep(1)  # wait a second for characteristics to be fully printed
                 for line in output_list[::-1]:
                     if "characteristics" in line.lower():
                         break
@@ -193,7 +206,8 @@ pu = subprocess.Popen(["/bin/hciconfig", "hci0", "up"])
 pu.wait()
 
 # connect to peripheral and execute command
-ps = subprocess.Popen(["/usr/bin/gatttool","-b",args.target,"-t","random","-I"], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+ps = subprocess.Popen(["/usr/bin/gatttool", "-b", args.target, "-t", "random", "-I"], stdout=subprocess.PIPE,
+                      stdin=subprocess.PIPE)
 t = thread.start_new_thread(parse_output, (ps.stdout,))
 connect(ps.stdin)
 if parse_connect():
@@ -207,11 +221,15 @@ if parse_connect():
             if args.command == cmd_get_battery:
                 read_characteristic(ps.stdin, char_dict[BATTERY_CHARACTERISTIC_UUID])
             elif args.command == cmd_get_position:
-                read_characteristic(ps.stdin, char_dict[MOTOR_STATE_CHARACTERISTIC_UUID]) # first byte in array is motor position
+                read_characteristic(ps.stdin,
+                                    char_dict[MOTOR_STATE_CHARACTERISTIC_UUID])  # first byte in array is motor position
             elif args.command == cmd_move_down:
                 write_characteristic(ps.stdin, char_dict[MOTOR_CONTROL_CHARACTERISTIC_UUID], MOTOR_MOVE_DOWN)
             elif args.command == cmd_move_up:
                 write_characteristic(ps.stdin, char_dict[MOTOR_CONTROL_CHARACTERISTIC_UUID], MOTOR_MOVE_UP)
+            elif args.command == cmd_move_target:
+                write_characteristic(ps.stdin, char_dict[MOTOR_TARGET_CHARACTERISTIC_UUID],
+                                     format(args.motor_target, "02x")) # convert input int to 2 digit hex string
             parse_read_write()
 disconnect(ps.stdin)
 ps.terminate()
