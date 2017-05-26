d4feb310b5c0767819a97383b19f1ac6d1de7203
Rego Aruorg <rego.aruorg@gmail.com>
2017-05-26 09:01:20 +0300

    Update websmartshades.py

--- websmartshades.py
+++ websmartshades.py
@@ -14,27 +14,27 @@ app = web.application(urls, globals())
 
 class get_battery:
     def GET(self, mac):
-        output = subprocess.check_output(['/usr/bin/python','/usr/src/smartblinds-diy/control.py', '-t', mac, '-c', 'get_battery'])
+        output = subprocess.check_output(['/usr/bin/python','./control.py', '-t', mac, '-c', 'get_battery'])
         return output
 
 class get_position:
     def GET(self, mac):
-        output = subprocess.check_output(['/usr/bin/python','/usr/src/smartblinds-diy/control.py', '-t', mac, '-c', 'get_position'])
+        output = subprocess.check_output(['/usr/bin/python','./control.py', '-t', mac, '-c', 'get_position'])
         return output
 
 class move_down:
     def GET(self, mac):
-        output = subprocess.check_output(['/usr/bin/python','/usr/src/smartblinds-diy/control.py', '-t', mac, '-c', 'move_down'])
+        output = subprocess.check_output(['/usr/bin/python','./control.py', '-t', mac, '-c', 'move_down'])
         return "OK"
 
 class move_up:
     def GET(self, mac):
-        output = subprocess.check_output(['/usr/bin/python','/usr/src/smartblinds-diy/control.py', '-t', mac, '-c', 'move_up'])
+        output = subprocess.check_output(['/usr/bin/python','./control.py', '-t', mac, '-c', 'move_up'])
         return "OK"
 
 class set_position:
     def GET(self, mac, position):
-        output = subprocess.check_output(['/usr/bin/python','/usr/src/smartblinds-diy/control.py', '-t', mac, '-c', 'move_target', '-a', position])
+        output = subprocess.check_output(['/usr/bin/python','./control.py', '-t', mac, '-c', 'move_target', '-a', position])
         return "OK"
 
 
